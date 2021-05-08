d0f8d5c31f4b1635db22d78718704007f8a92b37
Darkwind911 <maxxrph@gmail.com>
2021-05-08 15:28:40 +0300

    Update reject_created.sql

--- reject_created.sql
+++ reject_created.sql
@@ -1,11 +1,15 @@
 create schema rejected 
 
+drop table rejected .dim_aircrafts
+
 create  table rejected .dim_aircrafts
 (error_id serial,
 aircraft_code varchar(4) ,
 model varchar(50) , 
 aircraft_range int, 
 seats_total int  ) 
+
+drop table rejected.dim_airports
 
 create table rejected.dim_airports
 (airport_id serial ,
@@ -15,12 +19,16 @@ city varchar(100) ,
 timezone varchar(100) 
 )
 
+drop table rejected.dim_tariff
+
 create table rejected.dim_tariff
 (tarif_id serial ,
 tarif varchar(50) 
 )
 
-create table dds.dim_passenger
+drop table rejected.dim_passenger
+
+create table rejected.dim_passenger
 (passenger_id serial,
 passenger_name varchar(200) ,
 flight_id int , 
@@ -30,22 +38,23 @@ tarif int,
 costs int
 )
 
+drop table rejected.fact_flights
 
 create table rejected.fact_flights
 (passenger int  ,
-action_day int ,
-date_depature timestamp,
-date_arrival timestamp,
-delay_depature_sec int,
+arrival_date_id int ,
+depature_date_id int ,
+date_depature timestamp ,
+date_arrival timestamp ,
+delay_depature_sec int ,
 delay_arrival_sec int,
 aircraft_id int  ,
-airport_depature , 
-airport_arrival ,
+airport_depature int , 
+airport_arrival int ,
 tarif int  , 
 costs int  )
 
 
 
 
-SELECT * FROM dds.dim_airports
-where airport_code = 'DME'
+
